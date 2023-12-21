ecff540f4cfbd48e9f30cee349150ae699a473db
Voronin Egor <evoroni2004@gmail.com>
2023-12-21 18:32:46 +0300

    Добавил функцию печати разных типов.
    используя std::variant, теперь можно печатать разные типы так: print({ f, 15, 10.2, 65.4, "ban", 'f'});
    поддерживается для int. double, char, std::string, для c++17 и выше.
    
    Изменил постфиксный инкремент, на префиксный. Даёт большую производительность, для пользовательских типов.

--- CppLibrary/VoroninLib.h
+++ CppLibrary/VoroninLib.h
@@ -1,12 +1,13 @@
 #ifndef VORONIN_LIB_H_
 #define VORONIN_LIB_H_
 
-#include<functional>
-#include<iostream>
+#include <functional>
+#include <iostream>
+#include <variant>
 
 template<typename T>
 void print(const std::initializer_list<T>& il) {
-	for (auto it = il.begin(); it != il.end(); it++) {
+	for (auto it = il.begin(); it != il.end(); ++it) {
 		std::cout << *it << ' ';
 	}
 	std::cout << std::endl;
@@ -15,10 +16,30 @@ void print(const std::initializer_list<T>& il) {
 template<typename C>
 void print(const C& cont) {
 	auto last = std::prev(cont.end());
-	for (auto it = cont.begin(); it != last; it++) {
+	for (auto it = cont.begin(); it != last; ++it) {
 		std::cout << *it << ", ";
 	}
 	std::cout << *last << std::endl;
+}
+
+void print_v(const std::variant<int, double, std::string, char>& v) {
+	if (std::holds_alternative<int>(v))
+		std::cout << get<int>(v);
+	else if (std::holds_alternative<double>(v))
+		std::cout << get<double>(v);
+	else if (std::holds_alternative<std::string>(v))
+		std::cout << get<std::string>(v);
+	else if (std::holds_alternative<char>(v))
+		std::cout << get<char>(v);
+}
+
+void print(const std::initializer_list<std::variant<int, double, std::string, char>>& il) {
+	auto last = std::prev(il.end());
+	for (auto it = il.begin(); it < last; ++it) {
+		print_v(*it); std::cout << ", ";
+	}
+
+	print_v(*last);
 }
 
 

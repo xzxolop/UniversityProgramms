76aa4b47e214f34f8aa7de69c6ce4fe6fe9302a6
Voronin Egor <evoroni2004@gmail.com>
2024-01-12 21:50:13 +0300

    Добавил класс myvector

--- CppLibrary/VoroninLib/VoroninLib.h
+++ CppLibrary/VoroninLib/VoroninLib.h
@@ -48,6 +48,92 @@ void print(const std::initializer_list<std::variant<int, double, std::string, ch
 	print_v(*last);
 }
 
+template<typename T>
+class myvector {
+public:
+	myvector(int sz) : size(sz) {
+		data = new T[size];
+		for (int i = 0; i < size; i++) {
+			data[i] = T();
+		}
+	}
+
+	myvector(std::initializer_list<T> il) : size(il.size()) {
+		data = new T[size];
+		for (int i = 0; i < size; i++) {
+			data[i] = *(il.begin() + i);
+		}
+	}
+
+	myvector(const myvector& v) {
+		size = v.size;
+		data = new T[size];
+		for (int i = 0; i < size; i++) {
+			data[i] = v.data[i];
+		}
+	}
+
+	myvector& operator=(const myvector& v) {
+		if (this != &v) {
+			delete[] data;
+			size = v.size;
+			data = new T[size];
+			std::copy(v.data, v.data + v.size, data);
+			return *this;
+		}
+		return *this;
+	}
+
+	void swap(myvector v) {
+		std::swap(data, v.data);
+		std::swap(size, v.size);
+
+	}
+
+	myvector() = default;
+
+	myvector(myvector&& v) : myvector() {
+		swap(v);
+	}
+
+	myvector& operator=(myvector&& v) {
+		delete[] data;
+		swap(v);
+		v.data = nullptr;
+		v.size = 0;
+		return *this;
+	}
+
+	~myvector() {
+		if (data) {
+			delete[] data;
+		}
+	}
+
+	int length() const {
+		return size;
+	}
+
+	void push_back(T el) {
+		T* oldData = data;
+		data = new T[size + 1];
+		std::copy(oldData, oldData + size, data);
+		data[size] = el;
+		size++;
+		delete oldData;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const myvector& v) {
+		for (int i = 0; i < v.length(); i++) {
+			os << v.data[i] << ' ';
+		}
+		return os;
+	}
+
+private:
+	int size;
+	T* data;
+};
 
 
 #endif VORONIN_LIB_H_
